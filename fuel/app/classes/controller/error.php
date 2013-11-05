a595f2b37fefa5fdb4bbab799dbbc66ac4dbb2d7
Hugh Ybarra <hugh.ybarra@gmail.com>
2013-11-05 16:58:31 -0500

    added comments

--- fuel/app/classes/controller/error.php
+++ fuel/app/classes/controller/error.php
@@ -1,13 +1,11 @@
 <?php
 
-class Controller_Error extends Controller_Template
+class Controller_Error extends Controller
 {
 
 	public function action_404()
 	{
-		$data["subnav"] = array('404'=> 'active' );
-		$this->template->title = 'Error &raquo; 404';
-		$this->template->content = View::forge('error/404', $data);
+		echo "404 errors";
 	}
 
 }
