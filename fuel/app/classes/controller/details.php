ae678d544f54d73fe5eea831207033be3b1bd5d4
Hugh Ybarra <hugh.ybarra@gmail.com>
2013-11-05 16:54:24 -0500

    added comments

--- fuel/app/classes/controller/details.php
+++ fuel/app/classes/controller/details.php
@@ -5,7 +5,7 @@ class Controller_Details extends Controller
 
 	public function action_index()
 	{
-		echo "product details displays all the products in the database";
+		echo "product details displays all information on one product";
 		// $data["subnav"] = array('index'=> 'active' );
 		// $this->template->title = 'Details &raquo; Index';
 		// $this->template->content = View::forge('details/index', $data);
