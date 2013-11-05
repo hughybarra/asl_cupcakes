0e06e9987ef8a5d032e2a578dda91590d49d242e
Alan James <ajames@Landmine.fullsail.com>
2013-11-05 16:32:49 -0500

    Commit

---
+++ fuel/app/logs/2013/11/05.php
@@ -0,0 +1,6 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2013-11-05 16:30:23 --> Notice - Undefined variable: title in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/app/views/template.php on line 5
+ERROR - 2013-11-05 16:30:31 --> Notice - Undefined variable: title in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/app/views/template.php on line 5
+ERROR - 2013-11-05 16:30:34 --> Notice - Undefined variable: title in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/app/views/template.php on line 5
+ERROR - 2013-11-05 16:31:19 --> Notice - Undefined variable: title in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/app/views/template.php on line 5
