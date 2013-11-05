40aaaa66e043a9a2c12e88afd4e35501baf5ef16
Alan James <ajames@Landmine.fullsail.com>
2013-11-05 15:39:23 -0500

    Commit

--- fuel/app/logs/2013/11/05.php
+++ fuel/app/logs/2013/11/05.php
@@ -2,3 +2,21 @@
 
 ERROR - 2013-11-05 20:14:59 --> Error - date_default_timezone_get(): It is not safe to rely on the system's timezone settings. You are *required* to use the date.timezone setting or the date_default_timezone_set() function. In case you used any of those methods and you are still getting this warning, you most likely misspelled the timezone identifier. We selected 'America/New_York' for 'EST/-5.0/no DST' instead in /Applications/MAMP/htdocs/ASSL/cupcake/fuel/core/classes/fuel.php on line 161
 ERROR - 2013-11-05 20:16:17 --> Error - date_default_timezone_set(): Timezone ID 'America/New_York'' is invalid in /Applications/MAMP/htdocs/ASSL/cupcake/fuel/core/classes/fuel.php on line 161
+ERROR - 2013-11-05 15:24:19 --> Error - Could not find asset: bootstrap.css in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/core/classes/asset/instance.php on line 256
+ERROR - 2013-11-05 15:24:20 --> Error - Could not find asset: bootstrap.css in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/core/classes/asset/instance.php on line 256
+ERROR - 2013-11-05 15:26:30 --> Error - Could not find asset: bootstrap.css in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/core/classes/asset/instance.php on line 256
+ERROR - 2013-11-05 15:26:30 --> Error - Could not find asset: bootstrap.css in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/core/classes/asset/instance.php on line 256
+ERROR - 2013-11-05 15:31:43 --> Error - Could not find asset: bootstrap.css in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/core/classes/asset/instance.php on line 256
+ERROR - 2013-11-05 15:31:43 --> Error - Could not find asset: bootstrap.css in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/core/classes/asset/instance.php on line 256
+ERROR - 2013-11-05 15:33:12 --> Parsing Error - syntax error, unexpected '}' in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/app/classes/controller/comments.php on line 41
+ERROR - 2013-11-05 15:33:21 --> Parsing Error - syntax error, unexpected '}' in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/app/classes/controller/comments.php on line 41
+ERROR - 2013-11-05 15:33:26 --> Error - Could not find asset: bootstrap.css in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/core/classes/asset/instance.php on line 256
+ERROR - 2013-11-05 15:33:36 --> 2003 - SQLSTATE[HY000] [2003] Can't connect to MySQL server on '127.0.0.1' (61) in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/core/classes/database/pdo/connection.php on line 103
+ERROR - 2013-11-05 15:33:42 --> Error - Could not find asset: bootstrap.css in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/core/classes/asset/instance.php on line 256
+ERROR - 2013-11-05 15:33:46 --> Error - Could not find asset: bootstrap.css in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/core/classes/asset/instance.php on line 256
+ERROR - 2013-11-05 15:33:52 --> Error - Could not find asset: bootstrap.css in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/core/classes/asset/instance.php on line 256
+ERROR - 2013-11-05 15:35:15 --> Error - Could not find asset: bootstrap.css in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/core/classes/asset/instance.php on line 256
+ERROR - 2013-11-05 15:35:15 --> Error - Could not find asset: bootstrap.css in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/core/classes/asset/instance.php on line 256
+ERROR - 2013-11-05 15:36:17 --> Error - Could not find asset: bootstrap.css in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/core/classes/asset/instance.php on line 256
+ERROR - 2013-11-05 15:36:18 --> Error - Could not find asset: bootstrap.css in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/core/classes/asset/instance.php on line 256
+ERROR - 2013-11-05 15:36:27 --> Error - Could not find asset: bootstrap.css in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/core/classes/asset/instance.php on line 256
