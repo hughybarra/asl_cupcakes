c1d6c8b81f0e46f46292b9688628da43db69a0dc
Alan James <ajames@Landmine.fullsail.com>
2013-11-05 16:24:20 -0500

    Commit

--- fuel/app/classes/controller/users.php
+++ fuel/app/classes/controller/users.php
@@ -1,9 +1,9 @@
 <?php
 
-class Controller_Users extends Controller_Template
+class Controller_Users extends Controller
 {
 
-	public function action_login()
+	public function action_index()
 	{
     	// users will signup or signin here
 		echo "(users will signup or signin here)";
