d56380ebac3d8d4de290d690e982a9b7879ffcca
Alan James <ajames@Landmine.fullsail.com>
2013-11-05 18:43:47 -0500

    Commit

--- fuel/app/classes/controller/cart.php
+++ fuel/app/classes/controller/cart.php
@@ -5,8 +5,12 @@ class Controller_Cart extends Controller
 
 	public function action_index()
 	{
-		echo "cart functionality ";
-		echo "shows the cart lists products users have added to their cart";
+		//"cart functionality ";
+		//"shows the cart lists products users have added to their cart";
+		
+		$data['something'] = 'A testing variable';
+		
+		return View::forge('cart/index', $data);
 	}
 
 }
