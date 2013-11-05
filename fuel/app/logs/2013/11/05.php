854e3adb94b5afd4fc89c6af2b4a5f4f46b3a777
Alan James <ajames@Landmine.fullsail.com>
2013-11-05 18:45:23 -0500

    Commit

--- fuel/app/logs/2013/11/05.php
+++
@@ -1,3 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-ERROR - 2013-11-05 18:40:14 --> Notice - Undefined variable: data in /Users/ajames/Workspace/Aptana/ASL/asl_cupcakes/fuel/app/classes/controller/cart.php on line 11
