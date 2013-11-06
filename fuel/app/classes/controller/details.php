52bfcd9171efeb895f157cccbd8135e18b2c1ffe
Alan James <ajames@Landmine.fullsail.com>
2013-11-05 21:18:01 -0500

    Commit

--- fuel/app/classes/controller/details.php
+++ fuel/app/classes/controller/details.php
@@ -7,7 +7,7 @@ class Controller_Details extends Controller
 	{
 		// prepare a select statement to find product by passed Id
 		$data['product'] = Model_Product::find_by_id($product_id);
-		$this -> template -> content = View::forge('cart/index', $data);
+		$this -> template -> content = View::forge('details/index', $data);
 	}
 
 }
