9cac6f3db966959136e8a6fa20340b598bb18903
Alan James <ajames@Landmine.fullsail.com>
2013-11-06 10:16:15 -0500

    Commit

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -3,5 +3,6 @@ return array(
 	'_root_'  => 'index/index',  // The default route
 	'_404_'   => 'error/404',    // The main 404 route
 	
-	'details/:id' => 'details/index/$1'
+	'details/index/:id' => 'details/index/$1',
+	'details/:id' => 'details/index/$1',
 );
