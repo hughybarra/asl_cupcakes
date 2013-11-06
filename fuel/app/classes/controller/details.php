3d60816e0128ed7e9eb2fa836f4e3fbaf47caa3b
Alan James <ajames@Landmine.fullsail.com>
2013-11-05 21:17:18 -0500

    Commit

--- fuel/app/classes/controller/details.php
+++ fuel/app/classes/controller/details.php
@@ -3,18 +3,11 @@
 class Controller_Details extends Controller
 {
 
-	public function action_index()
+	public function action_index($product_id)
 	{
-		echo "product details displays all information on one product";
-		
-		// manually setting product Id. javascript should pass that data. 
-		$product_id = 1;
-		
 		// prepare a select statement to find product by passed Id
-		$query = Model_Product::find_by_id($product_id);
-		
-		// db working here
-		echo $query->name;
+		$data['product'] = Model_Product::find_by_id($product_id);
+		$this -> template -> content = View::forge('cart/index', $data);
 	}
 
 }
