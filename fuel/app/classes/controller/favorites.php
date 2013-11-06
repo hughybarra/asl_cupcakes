92157677ca49be09ae66ab7d906df3134affe5e7
Alan James <ajames@Landmine.fullsail.com>
2013-11-06 14:19:22 -0500

    Commit

--- fuel/app/classes/controller/favorites.php
+++ fuel/app/classes/controller/favorites.php
@@ -3,6 +3,38 @@ class Controller_Favorites extends Controller_Template {
 
 	public function action_index() {
 
+		// FAKE
+		$data['cart'] = array(
+			array(
+				'id' => '7',
+				'name' => 'Whatever',
+				'product_type' => 'something',
+				'product_description' => 'Something',
+				'price' => '2.50',
+				'image_path' => '7',
+				'quantity' => '2',
+				'product_reviews' => '',
+				'product_likes' => '',
+				'created_at' => '',
+				'updated_at' => ''
+			), 
+			array(
+				'id' => '6',
+				'name' => 'Whatever',
+				'product_type' => 'something',
+				'product_description' => 'Something',
+				'price' => '2.50',
+				'image_path' => '8',
+				'quantity' => '20',
+				'product_reviews' => '',
+				'product_likes' => '',
+				'created_at' => '',
+				'updated_at' => ''
+			)
+		);
+		
+		$this -> template -> content = View::forge('favorites/index', $data);
+
 	}
 
 }
