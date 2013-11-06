2e530f1112a0e373c0521f384faf9a50de3d7bb6
Alan James <ajames@Landmine.fullsail.com>
2013-11-06 10:15:13 -0500

    Commit

--- fuel/app/classes/controller/index.php
+++ fuel/app/classes/controller/index.php
@@ -1,32 +1,36 @@
 <?php
 
-class Controller_Index extends Controller_Template {
+class Controller_Index extends Controller_Template
+{
 
-	public function action_index() {
-
-		// products will be listed here
+	public function action_index()
+	{
+		// products will be listed here		
 		$data['products'] = Model_Product::find('all');
+		
 		$this -> template -> content = View::forge('index/index', $data);
-
 	}
 
 }
 
-/*
 
- / (products)
- /cart/
- /details/:id
- /favorites/
- /users/ (signup/login)
+/* 
 
- /action/logout
- /action/signup/
- /action/login/
- /action/add_to_cart
- /action/remove_from_cart
- /action/add_quantity
- /action/reduce_quantity
- /action/submit_order
+/ (products)
+/cart/
+/details/:id
+/favorites/
+/users/ (signup/login)
 
- */
+/action/logout
+/action/signup/
+/action/login/
+/action/add_to_cart
+/action/remove_from_cart
+/action/add_quantity
+/action/reduce_quantity
+/action/submit_order
+
+*/
+
+
--- fuel/app/classes/controller/users.php
+++ fuel/app/classes/controller/users.php
@@ -1,9 +1,11 @@
 <?php
 
-class Controller_Users extends Controller {
+class Controller_Users extends Controller
+{
 
-	public function action_index() {
-		// users will signup or signin here
+	public function action_index()
+	{
+    	// users will signup or signin here
 		echo "(users will signup or signin here)";
 	}
 
