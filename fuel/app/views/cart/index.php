10867aee2aafef3c529b685fdfd3ce2b912d8d34
Susan Brown <brownsusan@fullsail.edu>
2013-11-05 19:22:45 -0500

    commit

--- fuel/app/views/cart/index.php
+++ fuel/app/views/cart/index.php
@@ -1,3 +1,27 @@
-<p>
-	<?php echo $something; ?>
-</p>
+<div class="main">
+	<h1>Shopping Cart</h1>
+	<div class="shopping-cart">
+		<div>
+			<span>Total</span>
+			<span>$23.54</span>
+			<a href="#" class="button-submit">Checkout</a>
+		</div>
+	</div>
+	<div class="cart-items">
+		<?php for($i = 1; $i < 9; $i++):?>
+			<div class="cart-item">
+				<div class="cupcake-thumb">
+					<img src="/img/cupcakes/7.png">
+				</div>
+				<div class="cupcake-cart-info">
+					<span class="cupcake-name">Cupcake Name</span>
+					<span>Price</span>
+					<span>$2.50</span>
+					<span>Quantity</span>
+					<span>2</span>
+				</div>
+			</div>
+		<?php endfor; ?>
+	</div>
+	
+</div>
