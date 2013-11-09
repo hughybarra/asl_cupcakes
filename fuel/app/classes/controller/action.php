5e3e8b91f92e382bc5dd66fece97ef924aa174bd
Hugh Ybarra <hugh.ybarra@gmail.com>
2013-11-09 17:32:13 -0500

    added comments

--- fuel/app/classes/controller/action.php
+++ fuel/app/classes/controller/action.php
@@ -7,13 +7,11 @@ class Controller_Action extends Controller_Rest {
 	public function action_addReview() {
 		
 		/* 
-		 	add a new review to a product / Must be signed in
 		 
 		 	post vars:
 				user_review 
 		 		product_id
 		 	
-		 	user id is pulled from session variable. 
 		*/
 		
 		// validate 
@@ -52,6 +50,11 @@ class Controller_Action extends Controller_Rest {
 
 	public function action_signup() {
 		
+		/*	post vars:
+		 	username
+		 	password
+		 	email
+		*/
 		// validate
 		
 	    if(
@@ -90,6 +93,10 @@ class Controller_Action extends Controller_Rest {
 	}
 
 	public function action_login() {
+		/* post vars :
+		 email
+		 password
+		*/
 		
 		// validate
 	    if(
@@ -150,6 +157,11 @@ class Controller_Action extends Controller_Rest {
 
 	public function action_addToCart() {
 		
+		/*
+		 *	post vars: 
+		 *	item_id
+		 */
+		
 		// validate
 	    if(
 	    	!Input::post('item_id')
@@ -191,6 +203,12 @@ class Controller_Action extends Controller_Rest {
 	}
 
 	public function action_removeFromCart() {
+		
+		/*
+		 * post vars:
+		 * item_id
+		 * 
+		 */
 		
 		// validate
 	    if(
@@ -273,7 +291,11 @@ class Controller_Action extends Controller_Rest {
 
 	public function action_addFavorite()
 	{
-		
+		/* 
+		 * post vars :
+		 * product_id
+		 * 
+		 */	
 		// validate
 	    if(
 			!Input::post("product_id")
@@ -309,7 +331,10 @@ class Controller_Action extends Controller_Rest {
 
 	public function action_removeFavorite()
 	{
-
+		/*
+		 * post vars:
+		 * product_id
+		 */
 		// validate
 	    if(
 			!Input::post('product_id')
