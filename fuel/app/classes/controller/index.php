47828639ae519742008d94ca50ab047de9c27612
Alan James <ajames@Landmine.fullsail.com>
2013-11-12 20:00:54 -0500

    Commit

--- fuel/app/classes/controller/index.php
+++ fuel/app/classes/controller/index.php
@@ -1,64 +1,67 @@
 <?php
 
-class Controller_Index extends Controller_Template
-{
+class Controller_Index extends Controller {
 
-	public function action_index()
-	{
-		
-		// products will be listed here		
-		$data['products'] = Model_Product::find('all');
-		
-		$this -> template -> content = View::forge('index/index', $data);
-		
-		
+	public function action_index() {
+
 		//This is a necessary condition. It's advisable to stick the block in the constructor
 		//of your API controller
-		if(isset($_GET['en'])){
-			
-		//==============================EDIT THIS SECTION=============================================
-		
-			$app_id = '7b2cdade';//replace with your App Id in string form
-		
-		//==============================DO NOT EDIT THIS SECTION======================================
-		
+		if (isset($_GET['en'])) {
+
+			//==============================EDIT THIS SECTION=============================================
+
+			$app_id = '7b2cdade';
+			//replace with your App Id in string form
+
+			//==============================DO NOT EDIT THIS SECTION======================================
+
 			//encrypted data passed in the url on redirect
 			$userdata = $_GET['en'];
-		
+
 			//=====================Decryption Algorithm======================
-			if($userdata){
-				
+			if ($userdata) {
+
 				$decrypted = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($app_id), base64_decode(rawurldecode($userdata)), MCRYPT_MODE_CBC, md5(md5($app_id))), "\0");
-		
-			    //formatted so that he odd numbered indexes are the values
-			    //and even are the names for these values
-				$data_array = explode(',',$decrypted);
-		
-		
-		//===================Use these variables, and feel free to EDIT AFTER THIS POINT=================
-					
+
+				//formatted so that he odd numbered indexes are the values
+				//and even are the names for these values
+				$data_array = explode(',', $decrypted);
+
+				//===================Use these variables, and feel free to EDIT AFTER THIS POINT=================
+
 				//values interpreted for you already
 				$username = $data_array[0];
 				$name = $data_array[1];
 				$email = $data_array[2];
-		
-				var_dump($name);
+
+				$user = Model_User::find_by_user_emai($email);
 				
-				$my_user = array(
-					$username = $data_array[0],
-					$name = $data_array[1],
-					$email = $data_array[2]
-				);
+				// if the user is not created create them
+				if(!$user){
+					
+					$user = Model_User::forge(array(
+						'user_name' => $username, 
+						'user_pass' => '', 
+						'user_email' => $email
+					));
+					
+				}
 				
-				Session::set("test_user", $my_user );
-		
-				// echo $name . "  " . $username . "  " . $email;
-				// return "";
-				   
+				// start a session for the user
+				Session::set('user', array(
+					'id' => $user -> id, 
+					'user_name' => $user -> user_name, 
+					'user_email' => $user -> user_email
+				));
+
 			}// /endif
-		} // /en isset
+		}// /en isset
+
+		// products will be listed here
+		$data['products'] = Model_Product::find('all');
+
+		$this -> template -> content = View::forge('index/index', $data);
+
 	}
 
 }
-
-
--- fuel/app/classes/controller/madserv.php
+++ fuel/app/classes/controller/madserv.php
@@ -12,7 +12,7 @@ class Controller_Madserv extends Controller {
 		//This file is a model file. It handles the get requests and return data
 		//from the MadServ API for user authentication requests.
 		//
-		//You only need to edit your key and app id. Everything else has been handled for you.
+		//You only need to edit your key, app id, and redirect_to. Everything else has been handled for you.
 		//Happy Sailing!
 
 		//=======================================Edit This===================================
@@ -21,13 +21,16 @@ class Controller_Madserv extends Controller {
 		$key = '28f76f49fa9a7174d217b7928b99888a';
 		$app_id = '7b2cdade';
 
+		//This is YOUR site. This is where the user will be returned to after
+		//he/she has been authenticated by MadServ. Handle this however you'd like.
+		//DO NOT USE http:// in your url. It will break the API calls.
+		$redirect_to = 'group2.fsclass.info';
+
 		//=============================Don't edit anything below here========================
 
 		//built url, properly formatted for API
 		$url = 'http://madserv.us/';
 		$url .= '/api_request/validate_client/';
-		$url .= $key;
-		$url .= "/";
 		$url .= $app_id;
 
 		// 1. makes a REQUEST to the API to authenticate your app
@@ -38,18 +41,20 @@ class Controller_Madserv extends Controller {
 
 		$apiSecret = $decoded['secret'];
 		$redirectURL = $decoded['redirect'];
-
+		
+		// var_dump($request);
+		//----------authenticating secret------------------
 		//hashes your key & app_id to build your secret
 		//for API identification verification
 		$appSecret = md5($key);
 		$appSecret .= md5($app_id);
-
+		
 		// 3. checks secret key to be sure response was from the API
 		//and not from some evil hacker named Mike
 		if ($apiSecret == $appSecret) {
-
+			
 			// 4. upon success, the user is redirected to the API for authentication
-			header("Location: " . $redirectURL . "/" . $app_id);
+			return Response::redirect($redirectURL . "/" . $app_id . "/" . $redirect_to);
 
 		} else {
 			if (!$decoded) {
@@ -60,6 +65,7 @@ class Controller_Madserv extends Controller {
 
 			echo "Server response: " . $r;
 		}
+		
 	}
 
 	function action_userdata() {
